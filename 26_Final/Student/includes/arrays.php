65e848dcf6d24c2981ca30adb75b5938d579bdaf
rybaaa <bolshayaryba22@gmail.com>
2023-05-07 18:04:31 +0200

    feat: team page

--- 26_Final/Student/includes/arrays.php
+++ 26_Final/Student/includes/arrays.php
@@ -1,20 +1,42 @@
 <?php
-    $navItems = [
-        [
-            "slug" => "index.php",
-            "title" => "Home"
-        ],
-        [
-            "slug" => "team.php",
-            "title" => "Team"
-        ],
-        [
-            "slug" => "menu.php",
-            "title" => "Menu"
-        ],
-        [
-            "slug" => "contact.php",
-            "title" => "Contact"
-        ],
+$navItems = [
+    [
+        "slug" => "index.php",
+        "title" => "Home"
+    ],
+    [
+        "slug" => "team.php",
+        "title" => "Team"
+    ],
+    [
+        "slug" => "menu.php",
+        "title" => "Menu"
+    ],
+    [
+        "slug" => "contact.php",
+        "title" => "Contact"
+    ],
+];
+
+$teamMembers = [
+    [
+        "name" => "Frankie III",
+        "position" => "Owner",
+        "bio" => "Frankie is the great-grandson of the original Franklin. He is the owner of Franklin's Fine Dining. He cooks a mean fritatta!",
+        "img" => "frankie"
+    ],
+    [
+        "name" => "Francis",
+        "position" => "General Manager",
+        "bio" => "Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don't miss her Margherita Mondays!",
+        "img" => "francis"
+    ],
+    [
+        "name" => "Carlos",
+        "position" => "Head Chef",
+        "bio" => "Carlos is the epitome of the phrase &ldquo;Don't judge a book by it's cover&rdquo; &mdash; You simply cannot find a better chef.",
+        "img" => "carlos"
     ]
-?>
+]
+
+    ?>
--- 26_Final/Student/includes/footer.php
+++ 26_Final/Student/includes/footer.php
@@ -1,23 +1,29 @@
 <div id="footer" class="cf">
-                <div class="column three">
-                    <strong>Phone</strong>
-                    275.745.0499
-                </div>
-                <div class="column three">
-                    <strong>Location</strong>
-                    43 Jonh Avenue<br>
-                    Colorado, CL
-                </div>
-                <div class="column three last">
-                    <strong>Hours</strong>
-                    <em>Monday-Thursday</em><br>
-                    13:00 - 22:00<br><br>
-                    <em>Friday-Sunday</em><br>
-                    13:00 - 00:00<br><br>
-                </div>
-            </div>    
-            <small>&copy; <?php echo date("Y"); ?> Anton Rybak</small>        
-        </div>
-    </div>    
+    <div class="column three">
+        <strong>Phone</strong>
+        275.745.0499
+    </div>
+    <div class="column three">
+        <strong>Location</strong>
+        43 Jonh Avenue<br>
+        Colorado, CL
+    </div>
+    <div class="column three last">
+        <strong>Hours</strong>
+        <em>Monday-Thursday</em><br>
+        13:00 - 22:00<br><br>
+        <em>Friday-Sunday</em><br>
+        13:00 - 00:00<br><br>
+    </div>
+</div>
+<small>&copy;
+    <?php echo date("Y"); ?> Anton Rybak
+</small>
+</div>
+</div>
+<div class="copyright-info">
+    <?php include('../../assets/includes/copyright.php'); ?>
+</div>
 </body>
+
 </html>
